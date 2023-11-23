3a23e4f85dccbe4f7ed28bf098ba136671931bdb
ILOVEAIBECAUSEITISMYFAVOURITE <151824792+ILOVEAIBECAUSEITISMYFAVOURITE@users.noreply.github.com>
2023-11-23 14:16:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (name == "username") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
